074e01500fdf998737a707b9c8024c8ce5a34c70
Nikos Boumakis <csd4346@cd.uoc.gr>
2021-11-09 14:45:36 +0200

    microtcp_connect

--- lib/microtcp.c
+++ lib/microtcp.c
@@ -43,7 +43,9 @@ int microtcp_bind(microtcp_sock_t *socket, const struct sockaddr *address,
 
 int microtcp_connect(microtcp_sock_t *socket, const struct sockaddr *address,
                      socklen_t address_len) {
-    /* Your code here */
+    /**
+     *
+     */
 }
 
 int microtcp_accept(microtcp_sock_t *socket, struct sockaddr *address,
