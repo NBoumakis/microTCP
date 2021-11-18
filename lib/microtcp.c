5b154935abee45ff2dec1613297390d46a19e2e6
Konstantina Papafragkaki <csd4331@csd.uoc.gr>
2021-11-18 16:12:36 +0200

    commit -m "Add accept function"

--- lib/microtcp.c
+++ lib/microtcp.c
@@ -61,16 +61,40 @@ int microtcp_connect(microtcp_sock_t *socket, const struct sockaddr *address,
 
 int microtcp_accept(microtcp_sock_t *socket, struct sockaddr *address,
                     socklen_t address_len) {
-    /**
-     * call accept(socket->sd, ...)
-     * receive
-     * socket->ack = buffer->seq+1
-     * socket->seq = random()
-     * send(header)
-     * receive
-     * check
-     * socket->state = ESTABLISHED
-     * return 0 unless !bind || socket_invalid */
+    /** return 0 unless !bind || socket_invalid */
+    ssize_t bytes_recv,length;
+    void *buffer; //16bits of Control
+    microtcp_sock_t *sock;
+    microtcp_header_t *header;
+    if (accept(socket->sd,&address,address_len)!=-1){ //accept calls bind()??
+        //call accept(), create a socket from socket() and has address from bind(), returns sd
+        bytes_recv=microtcp_recv(&sock,&buffer,length,0); //receive (SYN=1,seq=N) packet
+        header->control=buffer; //16 bits
+        if (header->control[14]=='1' && header->seq_number==sock->seq_number){ //check SYN==1, seq_number==seq_number header
+            sock->seq_number=random(); //socket->seq=random()
+            sock->ack_number=header->seq_number+1; //socket->ack=buffer->seq+1
+            header->control[14]='1';
+            header->control[12]='1'; //make SYN=1,ACK=1
+            microtcp_send(sock,buffer,length,0); //send(header) SYN,ACK,seq=M,ack=N+1
+            bytes_recv=microtcp_recv(&sock,&buffer,length,0); //ACK, seq=N+1, ack=M+1
+            header->control=buffer;
+            if (header->control[12]=='1' && header->seq_number==sock->seq_number && header->ack_number==sock->ack_number){ 
+                //check ACK==1, seq=N+1, ack=M+1
+                socket->state=ESTABLISHED; //change state to ESTABLISHED
+                return socket->sd; //return the file descriptor of socket
+            } else{
+                printf("Wrong packet received by server!"); //error message
+                return -1; //return -1 for error
+            }
+        } else{
+            printf("Wrong packet received to server!"); //error message
+            return -1; //return -1 for error
+        }
+    } else{
+        printf("Accept() function cannot connect to OS! Error occured!");
+        //error message for accept()
+        return -1; //return -1 for errors
+    }
 }
 
 int microtcp_shutdown(microtcp_sock_t *socket, int how) {
