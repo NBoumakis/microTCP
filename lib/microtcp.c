04e962806999588516fa4d0e2ba8811f8efb38b5
Nikos Boumakis <csd4346@cd.uoc.gr>
2021-12-30 17:00:25 +0200

    microtcp_accept: More robust checking, use packet_header

--- lib/microtcp.c
+++ lib/microtcp.c
@@ -189,66 +189,70 @@ int microtcp_accept(microtcp_sock_t *socket, struct sockaddr *address,
     uint16_t ACK = 1 << 12;      // the last-3==1
     uint16_t SYNACK = SYN | ACK; // SYN=1, ACK=1
 
-    microtcp_header_t *header =
-        (microtcp_header_t *)malloc(sizeof(microtcp_header_t));
-
-    socket->recvbuf = (uint8_t *)malloc(sizeof(uint8_t) * MICROTCP_RECVBUF_LEN);
-    // socket->recvbuf([0-3]) refers to seq, [4-7] to ack
-    // and [8-9] in control
+    microtcp_header_t header;
 
     if (socket->state == INVALID) {
         printf("Socket invalid!\n");
         return -1;
     }
 
-    /*receive SYN=1, seq=N HEADER*/
-    if (recv(socket->sd, header, MICROTCP_RECVBUF_LEN, 0) == -1)
-        return -1;
-
-    if (header->control != SYN) { // check SYN sent from client
-        printf("Error with SYN, first packet from client\n!");
+    if (connect(socket->sd, address, address_len) == -1) {
         return -1;
     }
 
-    socket->ack_number = header->seq_number + 1;
-
-    // create the header that will sent to client
-    header->seq_number = socket->seq_number;
-    header->ack_number = socket->ack_number;
-    header->control = SYNACK;
-    header->window = MICROTCP_WIN_SIZE;
-    header->data_len = 0;
-    header->future_use0 = 0;
-    header->future_use1 = 0;
-    header->future_use2 = 0;
-    header->checksum = 0;
-
-    /*send SYN=1,ACK=1 from control, seq=M,ack=N+1 HEADER*/
-    if (send(socket->sd, header, sizeof(header), 0) == -1)
+    /*receive SYN=1, seq=N HEADER*/
+    if (recv(socket->sd, &header, MICROTCP_RECVBUF_LEN, 0) == -1)
         return -1;
+
+    if (crc32(&header, sizeof(header)) != header.checksum) {
+        printf("Error: 3-way handshake: SYN: Checksum\n");
+        return -1;
+    }
+
+    if (header.control != SYN) { // check SYN sent from client
+        printf("Error: 3-way handshake: SYN: Control field\n");
+        return -1;
+    }
+
+    socket->ack_number = header.seq_number + 1;
+
+    packet_header(&header, socket->seq_number, socket->ack_number, 1, 0, 1, 0, MICROTCP_WIN_SIZE, 0, 0, 0, 0, 0);
+    header.checksum = crc32(&header, sizeof(header));
+
+    /*send SYN=1, ACK=1 from control, seq=M,ack=N+1 HEADER*/
+    if (send(socket->sd, &header, sizeof(header), 0) == -1)
+        return -1;
+
+    ++socket->seq_number;
 
     /*receive ACK=1 from control, seq=N+1,ack=M+1 HEADER*/
-    if (recv(socket->sd, header, MICROTCP_RECVBUF_LEN, 0) == -1)
+    if (recv(socket->sd, &header, MICROTCP_RECVBUF_LEN, 0) == -1)
         return -1;
 
-    if (header->control != ACK) { // check ACK from client, second packet recv
-        printf("Error with ACK,second packet receive from server!\n");
-        return -1;
-    }
-
-    if (header->seq_number !=
-        socket->ack_number) { // N+1 from header equals seq+1 that server sent
-        printf("Error with seq (N+1) sent!\n");
+    if (crc32(&header, sizeof(header)) != header.checksum) {
+        printf("Error: 3-way handshake: ACK: Checksum\n");
         return -1;
     }
 
-    if (header->ack_number !=
-        socket->seq_number + 1) { // M+1 from header equals ack that server sent
-        printf("Error with seq (M+1) sent!\n");
+    if (header.control != ACK) { // check ACK from client, second packet recv
+        printf("Error: 3-way handshake: ACK: Control fields\n");
         return -1;
     }
 
-    free(header);
+    if (header.seq_number != socket->ack_number) { // N+1 from header equals seq+1 that server sent
+        printf("Error: 3-way handshake: ACK: Seq number\n");
+        return -1;
+    }
+
+    if (header.ack_number != socket->seq_number) { // M+1 from header equals ack that server sent
+        printf("Error: 3-way handshake: ACK: Ack number\n");
+        return -1;
+    }
+
+    ++socket->ack_number;
+    socket->curr_win_size = header.window;
+
+    socket->recvbuf = (uint8_t *)malloc(sizeof(uint8_t) * MICROTCP_RECVBUF_LEN);
 
     return 0; // return 0 for success
 }
